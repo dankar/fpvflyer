9b0998c04e2f71a5777360ae1823e8320fcad403
unknown <Roni01@CHMC-2047.ad.comhem.com>
2013-11-15 23:08:02 +0100

    Debug.Log messages for pressed keys

--- Assets/Scripts/MainMenu.js
+++ Assets/Scripts/MainMenu.js
@@ -1,15 +1,30 @@
 ﻿#pragma strict
 
+var is_visible = false;
+
 function Start () {
 
 }
 
 function Update () {
-
+	/*if ( !is_visible && Input.GetKey ("joystick button 11") ) is_visible = true;
+	else if ( is_visible && Input.GetKey("joystick button 12") ) is_visible = false; */
+	for( var i=1; i < 13; i++ ) {
+		if( Input.GetKeyUp("joystick button "+i) ) {
+			Debug.Log("Btn " + i + " pressed");
+		 }
+	}
 }
 
 function OnGUI () {
-	if (GUI.Button (Rect (10,10,150,100), "I am a button")) {
-		print ("You clicked the button!");
-	}
+
+	
+	/*if ( is_visible ) {
+		GUI.Box(Rect(10,10,200,500), "Inställningar");
+		
+		if (GUI.Button (Rect (20,50,180,30), "Starta om")) {
+			print ("You clicked the button!");
+		}
+	}*/
+	
 }
