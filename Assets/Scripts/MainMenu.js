9ee3a542ac559813b03cb7d09f1143d77cd82eb8
Robin Nilsson <robinnilsson@gmail.com>
2013-11-16 00:12:01 +0100

    New scene for main menu

--- Assets/Scripts/MainMenu.js
+++ Assets/Scripts/MainMenu.js
@@ -1,30 +1,26 @@
 ﻿#pragma strict
 
-var is_visible = false;
+var scenes = 	[	
+					['scene1','Första banan'],
+					['scene2','Andra banan']
+				];
 
 function Start () {
 
 }
 
 function Update () {
-	/*if ( !is_visible && Input.GetKey ("joystick button 11") ) is_visible = true;
-	else if ( is_visible && Input.GetKey("joystick button 12") ) is_visible = false; */
-	for( var i=1; i < 13; i++ ) {
-		if( Input.GetKeyUp("joystick button "+i) ) {
-			Debug.Log("Btn " + i + " pressed");
-		 }
-	}
+
 }
 
 function OnGUI () {
-
 	
-	/*if ( is_visible ) {
-		GUI.Box(Rect(10,10,200,500), "Inställningar");
-		
-		if (GUI.Button (Rect (20,50,180,30), "Starta om")) {
-			print ("You clicked the button!");
+	GUI.Box(Rect(10,10,100,500),"FPV Flyer");
+	
+	for(var i=0;i<scenes.length;i++) {
+		if (GUI.Button (Rect (20,50+35*i,180,30), scenes[i][1])) {
+			Application.LoadLevel(scenes[i][0]);
 		}
-	}*/
-	
+	}
+
 }
