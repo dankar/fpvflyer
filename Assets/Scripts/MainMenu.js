1fecb0ecdfe9b8d650c8340700eb1b96a131b064
Robin Nilsson <robinnilsson@gmail.com>
2013-11-16 22:42:49 +0100

    Stereoscopic GUI in main menu

--- Assets/Scripts/MainMenu.js
+++ Assets/Scripts/MainMenu.js
@@ -10,8 +10,9 @@ private var stateMain : int = 1;
 private var stateOptions : int = 2;
 private var stateCalibration : int = 3;
 private var state : int = stateMain;
-
-
+private var center_x : int = Screen.width/2/2;
+private var center_x_2 : int = Screen.width/2/2*3;
+private var center_y : int = Screen.height/2;
 
 function Start () {
 
@@ -23,53 +24,67 @@ function Update () {
 
 function drawMainMenu()
 {	
-	for(var i=0;i<scenes.length;i++) {
-		if (GUI.Button (Rect (20,50+35*i,180,30), scenes[i][1])) {
-			Application.LoadLevel(scenes[i][0]);
-		}
-	}
+	// Both left and right screen
+	for(cur_center_x in [center_x,center_x_2]) {
 	
-	if (GUI.Button (Rect (20,50+35*i,180,30), "Options")) {
-		state = stateOptions;
+		// One button per level
+		for(var i=0;i<scenes.length;i++) {
+			
+			if (GUI.Button (Rect (cur_center_x-90+20,center_y-15+50+35*i,180,30), scenes[i][1])) {
+				Application.LoadLevel(scenes[i][0]);
+			}
+
+		}
+		
+		if (GUI.Button (Rect (cur_center_x-90+20,center_y-15+50+35*i,180,30), "Options")) {
+			state = stateOptions;
+		}
+		
 	}
 }
 
 function drawCalibrationMenu()
 {
+
 	var yaw = Input.GetAxis("yaw");
 	var collective = Input.GetAxis("collective");
 	var pitch = Input.GetAxis("pitch");
 	var roll = Input.GetAxis("roll");
 	
-	GUI.TextArea(Rect (20,50+35*0,180,30), yaw.ToString());
-	GUI.TextArea(Rect (20,50+35*1,180,30), collective.ToString());
-	GUI.TextArea(Rect (20,50+35*2,180,30), pitch.ToString());
-	GUI.TextArea(Rect (20,50+35*3,180,30), roll.ToString());
-	
-	
-	if (GUI.Button (Rect (20,50+35*4,180,30), "Save")) {
-		state = stateOptions;
+	// Both left and right screen
+	for(cur_center_x in [center_x,center_x_2]) {
+		GUI.TextArea(Rect (cur_center_x-90+20,center_y-15+50+35*0,180,30), yaw.ToString());
+		GUI.TextArea(Rect (cur_center_x-90+20,center_y-15+50+35*1,180,30), collective.ToString());
+		GUI.TextArea(Rect (cur_center_x-90+20,center_y-15+50+35*2,180,30), pitch.ToString());
+		GUI.TextArea(Rect (cur_center_x-90+20,center_y-15+50+35*3,180,30), roll.ToString());
+		
+		if (GUI.Button (Rect (cur_center_x-90+20,center_y-15+50+35*4,180,30), "Save")) {
+			state = stateOptions;
+		}
+		if (GUI.Button (Rect (cur_center_x-90+20,center_y-15+50+35*5,180,30), "Cancel")) {
+			state = stateOptions;
+		}
 	}
-	if (GUI.Button (Rect (20,50+35*5,180,30), "Cancel")) {
-		state = stateOptions;
-	}
-	
+
 }
 
 function drawOptionsMenu()
 {
-	if (GUI.Button (Rect (20,50+35*0,180,30), "Calibrate controller")) {
-		state = stateCalibration;
-	}
-	
-	if (GUI.Button (Rect (20,50+35*1,180,30), "Back")) {
-		state = stateMain;
+
+	// Both left and right screen
+	for(cur_center_x in [center_x,center_x_2]) {
+		if (GUI.Button (Rect (cur_center_x-90+20,center_y-15+50+35*0,180,30), "Calibrate controller")) {
+			state = stateCalibration;
+		}
+		
+		if (GUI.Button (Rect (cur_center_x-90+20,center_y-15+50+35*1,180,30), "Back")) {
+			state = stateMain;
+		}
 	}
 }
 
 function OnGUI () {
-	GUI.Box(Rect(10,10,100,500),"FPV Flyer");
-	
+
 	switch(state)
 	{
 		case stateMain:
