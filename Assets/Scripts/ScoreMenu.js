1e5e6e9509154e923252ce0587c74f540f847575
Robin Nilsson <robinnilsson@gmail.com>
2013-11-17 17:14:13 +0100

    ScoreScreen is now controllable without mouse

--- Assets/Scripts/ScoreMenu.js
+++ Assets/Scripts/ScoreMenu.js
@@ -1,13 +1,20 @@
 ﻿#pragma strict
 
 private var scoreKeeper : ScoreKeeper;
+private var playerInput : PlayerInput;
 
 function Start () {
+	playerInput = GetComponent(PlayerInput);
 	scoreKeeper = GameObject.Find("ScoreKeeperObject").GetComponent("ScoreKeeper");	
 }
 
 function Update () {
-
+	
+	// Go to main menu on press of x or start
+	if(Input.GetKeyDown(playerInput.getMappedKeyCode("start")) || Input.GetKeyDown(playerInput.getMappedKeyCode("x"))) {
+		Application.LoadLevel("MainMenu");
+	}
+	
 }
 
 function drawScoreMenu()
@@ -15,6 +22,7 @@ function drawScoreMenu()
 
 	GUI.TextArea(Rect (20,50+35*0,180,30), (scoreKeeper.LevelEnd - scoreKeeper.LevelStart).ToString());
 	if (GUI.Button (Rect (20,50+35*1,180,30), "Main Menu")) {
+		// The player can also press x or start to get to the main menu
 		Application.LoadLevel("MainMenu");
 	}
 }
